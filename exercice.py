9d9420292c1e7aae07e27400e820ac608df9b009
AstraZenecaLeMedoc <105372262+AstraZenecaLeMedoc@users.noreply.github.com>
2022-09-13 10:22:52 -0400

    v2

--- exercice.py
+++ exercice.py
@@ -3,18 +3,21 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	puissance = voltage**2 / resistance
+	return puissance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	#v1[0] # Pour accéder au X
+	#v1[1] # Pour accéder au Y
+	ortho = v1[0]*v2[0] + v1[1]*v2[1]
+	return ortho == 0
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
-		pass # La variable v contient une valeur de la liste.
+	for i in range(len(values)):
+		values[i] = int(values[i])
+		print("La moyenne est :", sum(values) / len(values))
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
